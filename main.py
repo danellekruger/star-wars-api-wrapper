93466302441ebc0fb3160e4f9a4d24ff5f9e6dfe
Kru9erD <kruger.danelle2829@gmail.com>
2025-09-25 14:33:37 +0200

    Initial commit, imports

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+import uvicorn
+from services.swapi_service import SWAPIService
+from services.cache_service import CacheService
+from models.responses import FilmListResponse, CharacterListResponse, StarshipListResponse
+import logging
+from contextlib import asynccontextmanager
